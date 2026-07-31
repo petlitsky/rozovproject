13564969131dc109b81c7db898c3c0e04a5254dd
Amaratik <rpetlisky@gmail.com>
2026-07-31 20:17:49 +0300

    last

--- ui/main_window.py
+++ ui/main_window.py
@@ -904,7 +904,7 @@ class MainWindow(QMainWindow):
         print(f"[Post] Разжатие до {target_moment:.2f} Н·м")
         
         self._post_moving_back = True
-        self._post_target_moment = target_moment
+        self._post_target_moment = target_moment 
 
     def post_home(self) -> None:
         self.show_homing_dialog("Поиск дома постобжима...")
