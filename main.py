e1cfc53acc08e24c87bf2b9a7f32520831a9e5e7
Amaratik <rpetlisky@gmail.com>
2026-07-31 18:05:34 +0300

    real last version

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
 import sys
-import os
 from PySide6.QtWidgets import QApplication
 from ui.main_window import MainWindow
 from PySide6.QtCore import Qt
 
 
 def main() -> None:
-    os.environ["QT_IM_MODULE"] = "qtvirtualkeyboard"
-    os.environ["QT_VIRTUALKEYBOARD_AUTO_HIDE"] = "0"
 
     app = QApplication(sys.argv)
-
-    app.setAttribute(Qt.ApplicationAttribute.AA_EnableHighDpiScaling, True)
 
     window = MainWindow()
     window.show()
 
-    app.focusChanged.connect(_on_focus_changed)
-
     sys.exit(app.exec())
-
-def _on_focus_changed(old, new):
-    if new and hasattr(new, 'echoMode'):
-        QGuiApplication.inputMethod().show()
-        QGuiApplication.inputMethod().update(Qt.ImQueryAll)
 
 if __name__ == "__main__":
     main()
